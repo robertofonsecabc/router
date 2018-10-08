38b2be14db4ff94fe067946fa37c99f65c1798f9
Roberto Fonseca <roberto.alves@canaldapeca.com.br>
2018-10-08 14:55:13 -0300

    Criando rota para Customer

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# router
+# Router Integration Project
 Roteador
+
+
+## Dependencias
+
+### ActiveMQ
+$ cd bin/
+$ sh activemq console
+
+Acessar http://0.0.0.0:8161/ 
+Usuário admin
+Senha admin
---
+++ src/main/java/br/com/canaldapeca/router/routers/CustomerRouter.java
@@ -0,0 +1,40 @@
+package br.com.canaldapeca.router.routers;
+
+
+import com.google.gson.Gson;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import java.util.Map;
+
+@Component("customerRouter")
+public class CustomerRouter  {
+
+    @JmsListener(destination = "sample.queue")
+    public void receiveQueue(String text) {
+        System.out.println(text);
+    }
+
+    @JmsListener(destination = "inbound.topic")
+    @SendTo("outbound.topic")
+    public String receiveMessageFromTopic(final Message jsonMessage) throws JMSException {
+        String messageData = null;
+        System.out.println("Received message " + jsonMessage);
+        String response = null;
+
+        if(jsonMessage instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage)jsonMessage;
+            messageData = textMessage.getText();
+            Map map = new Gson().fromJson(messageData, Map.class);
+            response  = "Hello " + map.get("name");
+        }
+
+        return response;
+    }
+
+}
