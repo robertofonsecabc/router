2dc4d5ba87a4e93134a33cba0330399e77416dc6
Roberto Fonseca <roberto.alves@canaldapeca.com.br>
2018-10-08 16:45:35 -0300

    Adicionando H2 Database

--- src/main/java/br/com/canaldapeca/router/RouterApplication.java
+++ src/main/java/br/com/canaldapeca/router/RouterApplication.java
@@ -4,6 +4,7 @@ package br.com.canaldapeca.router;
 import org.apache.camel.CamelContext;
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
@@ -18,11 +19,13 @@ public class RouterApplication {
     @Autowired
     private CamelContext context;
 
+    @Value("${spring.activemq.broker-url}")
+    private String broker_url;
 
 
     @PostConstruct
     public void init() throws Exception {
-        context.addComponent("jms", ActiveMQComponent.activeMQComponent("tcp://localhost:61616"));
+        context.addComponent("jms", ActiveMQComponent.activeMQComponent(broker_url));
     }
 
     public static void main(String[] args) {
